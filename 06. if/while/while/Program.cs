a5a66a424fb64774d57706638b8039b23e30aeff
noVlehsoP <noVlehsoP@users.noreply.github.com>
2018-02-15 18:00:27 +0300

    ot strix

---
+++ 06. if/while/while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                sec = sec + 1;
+                Console.WriteLine(sec);
+            }
+
+        }
+    }
+}
